cded145536c6d1d73764581be19de8d47ec25013
Madhav008 <madhavj211@gmail.com>
2024-03-15 00:08:25 +0530

    cron seeding time added

--- api/routes/cron.js
+++ api/routes/cron.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const apiController = require('../controller/croncontroller');
 
 // Example protected API route
-router.get('/seed/matches', apiController.seedMatches);
-router.get('/seed/players', apiController.seedPlayers);
+router.get('/seed/matches', apiController.seedMatches); //seed every 2hrs
+router.get('/seed/players', apiController.seedPlayers); //seed every 2hrs
 router.get('/seed/login', apiController.login);
-router.get('/seed/stats', apiController.seedPlayersStats);
+router.get('/seed/stats', apiController.seedPlayersStats); //seed every 30min for live matches and for completed 1time
 
 
 
