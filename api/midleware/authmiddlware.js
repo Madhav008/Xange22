2ee7fbaaee9a13e2bb9e1d66f849a1952eb3a107
Madhav008 <madhavj211@gmail.com>
2024-03-30 14:57:43 +0530

    Added the adminpannel routes

--- api/midleware/authmiddlware.js
+++ api/midleware/authmiddlware.js
@@ -25,13 +25,29 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next()
-  } else {
-    res.status(401).json({ message: "Not authorized as an admin" })
-
+const admin = asyncHandler(async (req, res, next) => {
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      let user = await User.findById(decoded.userId).select('-password')
+      if (user.isAdmin) {
+        next()
+      } else {
+        res.status(401).json({ message: "Not authorized as an admin" })
+        throw new Error('Not authorized')
+      }
+    } catch (error) {
+      console.error(error)
+      res.status(401).json({ message: "Not authorized" })
+      throw new Error('Not authorized, token failed')
+    }
   }
-}
+
+})
 
 module.exports = { protect, admin }
