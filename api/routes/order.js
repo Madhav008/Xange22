06de56c4af2ac181ba855518204f8f8f32d5ed9f
Madhav008 <madhavj211@gmail.com>
2024-03-30 15:05:18 +0530

    addded thatt

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const apiController = require('../controller/orderscontroller');
-const { protect } = require('../midleware/authmiddlware');
+const { protect, admin } = require('../midleware/authmiddlware');
 
 // Example protected API route
 router.post('/create', protect, apiController.createOrder);
 router.get('/:userId', protect, apiController.getUserOrders);
+router.post('/', admin, apiController.getOrders);
+router.get('/match/:matchId', admin, apiController.getMatchOrders);
+
 
 
 
